b6deea85cec769e043862561e742e3750e1186ec
Artur Burtsev <artjock@gmail.com>
2013-03-31 20:48:36 +0400

    model create

--- cool.js
+++ cool.js
@@ -458,6 +458,7 @@ util.extend(cool.view, statik);
 cool._models = {};
 
 var proto = {
+
     _init: function(params) {
         if (!this.params) {
             this.params = params;
@@ -467,33 +468,49 @@ var proto = {
 
         return this;
     },
+
     data: function(raw) {
         return raw ? this._data : this.process(this.parse(this._data));
     },
+
     parse: function(data) {
         return data.result;
     },
+
     process: function(data) {
         return data;
     },
+
     read: function(params) {
         return this.sync({
+            type: 'get',
             params: util.extend(this.params, params || {})
         });
     },
+
+    create: function(params) {
+        return this.sync({
+            type: 'post',
+            params: util.extend(this.params, params || {})
+        });
+    },
+
     sync: function(options) {
-        var that = this;
         var promise = cool.promise();
+        var type = options.type;
+        var params = options.params;
 
         $.ajax({
             url: this.url,
             traditional: true,
-            data: options.params,
+            type: type,
+            data: params,
+            context: this,
             dataType: 'json',
             success: function(data) {
-                that._data = data;
-                promise.resolve(that);
-                that.trigger('read', that.data());
+                this._data = data;
+                promise.resolve(this);
+                this.trigger('read', this.data());
             }
         });
 
