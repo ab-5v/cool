d3533e5f6f94f3aa7d990276d080e5329d44857d
Artur Burtsev <artjock@gmail.com>
2013-05-29 13:19:58 +0400

    events tests skeletons

---
+++ test/spec/cool.events.js
@@ -0,0 +1,91 @@
+describe('cool.events', function() {
+
+    describe('_event', function() {
+
+        it('should create event by `type`', function() {
+            
+        });
+
+        it('should ensure `event` has `owner`', function() {
+            
+        });
+
+        it('should extend event by `extra`', function() {
+            
+        });
+
+    });
+
+    describe('on', function() {
+
+        it('should throw if listener is not a function', function() {
+            
+        });
+
+        it('should ensure events store', function() {
+            
+        });
+
+        it('should add listener to the store', function() {
+            
+        });
+
+        it('should add context to the store', function() {
+            
+        });
+
+        it('should add `this` as a context if no one specified', function() {
+            
+        });
+
+        it('should return `this`', function() {
+            
+        });
+
+    });
+
+    describe('off', function() {
+
+        it('should skip if no store found', function() {
+            
+        });
+
+        it('should remove all listeners by type', function() {
+            
+        });
+
+        it('should remove by listener', function() {
+            
+        });
+
+        it('should remove by context', function() {
+            
+        });
+
+        it('should return `this`', function() {
+            
+        });
+
+    });
+
+    describe('emit', function() {
+
+        it('shoult call _event if type passed', function() {
+            
+        });
+
+        it('should call every istener of specified type', function() {
+            
+        });
+
+        it('should pass event object', function() {
+            
+        });
+
+        it('should pass data object', function() {
+            
+        });
+
+    });
+
+});
