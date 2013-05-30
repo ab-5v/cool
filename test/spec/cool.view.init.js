1a4a0f6ed436cedf6ea69b980add01255b109384
Artur Burtsev <artjock@gmail.com>
2013-05-30 21:46:27 +0400

    init.models

--- test/spec/cool.view.init.js
+++ test/spec/cool.view.init.js
@@ -1,10 +1,36 @@
 describe('cool.view.init', function() {
 
-    var init = cool.view.init;
+    var init= cool.view.init;
+
+    beforeEach(function() {
+
+        this.to = function(ms, val) {
+            var promise = cool.promise();
+            setTimeout(function() {
+                val.data({t: ms});
+                promise.resolve(val);
+            }, ms);
+            return promise;
+        };
+
+        this.view = new cool.view();
+        this.view.data({a: 1});
+        this.view.params({b: 2});
+    });
+
+    afterEach(function() {
+        cool.view._insts = {};
+        cool.view._ctors = {};
+        cool.model._insts = {};
+        cool.model._ctors = {};
+
+    });
 
     describe('views', function() {
 
         beforeEach(function() {
+            var to = this.to;
+
             cool.view({name: 'v1'});
             cool.view({name: 'v2'});
             cool.view({name: 'async', models: ['async']});
@@ -12,17 +38,9 @@ describe('cool.view.init', function() {
             cool.model({
                 name: 'async',
                 url: '/test',
-                fetch: function() {
-                    var t = this;
-                    var p = pzero();
-                    setTimeout(function() { p.resolve(t); }, 10);
-                    return p;
-                }
+                fetch: function() { return to(10, this); }
             });
 
-            this.view = new cool.view();
-            this.view.data({a: 1});
-            this.view.params({b: 2});
             this.view.views = ['v1', 'v2'];
 
             sinon.spy(cool, 'view');
@@ -30,12 +48,6 @@ describe('cool.view.init', function() {
 
         afterEach(function() {
             cool.view.restore();
-
-            cool.view._insts = {};
-            cool.view._ctors = {};
-            cool.model._insts = {};
-            cool.model._ctors = {};
-
         });
 
         it('should create specified views', function() {
@@ -102,6 +114,94 @@ describe('cool.view.init', function() {
                 done();
             }, 10);
         });
+
+    });
+
+    describe('models', function() {
+
+        beforeEach(function() {
+            var to = this.to;
+
+            cool.model({
+                name: 'a1', url: '/test',
+                fetch: function() { return to(10, this); }
+            });
+
+            cool.model({
+                name: 'a2', url: '/test',
+                fetch: function() { return to(20, this); }
+            });
+
+            this.view.models = ['a1', 'a2'];
+
+            sinon.spy(cool, 'model');
+        });
+
+        afterEach(function() {
+            cool.model.restore();
+        });
+
+        it('should create specified models', function() {
+            init.models(this.view);
+
+            expect( cool.model.calledTwice ).to.be.ok();
+
+            expect( cool.model.returnValues[0] ).to.be.a( cool.model );
+            expect( cool.model.returnValues[0].name ).to.be( 'a1' );
+            expect( cool.model.returnValues[1] ).to.be.a( cool.model );
+            expect( cool.model.returnValues[1].name ).to.be( 'a2' );
+        });
+
+        it('should store specified models', function() {
+            init.models(this.view);
+
+            expect( this.view.models['a1'].name ).to.be( 'a1' );
+            expect( this.view.models['a2'].name ).to.be( 'a2' );
+        });
+
+        it('should pass params to each model', function() {
+            init.models(this.view);
+
+            expect( this.view.models['a1'].params() ).to.eql( {b: 2} );
+            expect( this.view.models['a2'].params() ).to.eql( {b: 2} );
+        });
+
+        it('should pass data to each model', function() {
+            init.models(this.view);
+
+            expect( this.view.models['a1'].data() ).to.eql( {a: 1} );
+            expect( this.view.models['a2'].data() ).to.eql( {a: 1} );
+        });
+
+
+        it('should call `fetch` for each model', function() {
+            sinon.spy(cool.model.prototype, 'fetch');
+            cool.model({name: 't', url: '/'});
+            this.view.models = ['t'];
+            init.models(this.view);
+
+            expect( cool.model.prototype.fetch.calledOnce ).to.be.ok();
+
+            cool.model.prototype.fetch.restore();
+        });
+
+        it('should return promise', function() {
+            expect( cool.promise.is(init.models(this.view)) ).to.be.ok();
+        });
+
+        it('should resolve, when all models are fetched', function(done) {
+            var view = this.view;
+
+            init.models(view)
+                .then(function() {
+                    console.log(arguments);
+
+                    expect( view.models['a1'].data() ).to.eql( {t: 10} );
+                    expect( view.models['a2'].data() ).to.eql( {t: 20} );
+                    done();
+                });
+        });
+
 
     });
 
