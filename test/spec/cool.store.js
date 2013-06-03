210a30f0fe7ca4c0435cc60d2ce4440c044c0388
Artur Burtsev <artjock@gmail.com>
2013-06-03 18:18:32 +0400

    store api tests

--- test/spec/cool.store.js
+++ test/spec/cool.store.js
@@ -3,25 +3,74 @@ describe('store', function() {
     beforeEach(function() {
         this.aim = {};
         xtnd(this.aim, cool.store('data'));
+        this.aim._data = {};
     });
 
-    it('should return object', function() {
+    it('should construct store', function() {
         var store = cool.store('data');
 
         expect( store ).to.be.an( Object );
         expect( store.data ).to.be.a( Function );
     });
 
-    it('should set data', function() {
+    it('should work for `data({})`', function() {
         this.aim.data( {a: 1} );
+        this.aim.data( {b: 2} );
 
-        expect( this.aim._data ).to.eql( {a: 1} );
+        expect( this.aim._data ).to.eql( {b: 2} );
     });
 
-    it('should return data', function() {
-        this.aim._data = {b: 2};
+    it('should work for `data(true, {})`', function() {
+        this.aim.data( {a: 1} );
+        this.aim.data( true, {b: 2} );
+
+        expect( this.aim.data() ).to.eql( {a: 1, b: 2} );
+    });
+
+    it('should work for `data(name, value)`', function() {
+        this.aim.data('a', 2);
+
+        expect( this.aim.data() ).to.eql( {a: 2} );
+    });
+
+    it('should work for `data(true, name, value)`', function() {
+        this.aim.data({a: [1, 2]});
+        this.aim.data(true, 'a', 3);
+
+        expect( this.aim.data() ).to.eql( {a: [1, 2, 3]} );
+    });
+
+    it('should work for `data(false)`', function() {
+        this.aim.data({a: 1});
+        this.aim.data(false);
+
+        expect( this.aim.data() ).to.eql( {} );
+    });
+
+    it('should work for `data(false, name)`', function() {
+        this.aim.data({a: 1, b: 2});
+        this.aim.data(false, 'a');
 
         expect( this.aim.data() ).to.eql( {b: 2} );
+    });
+
+    it('should work for `data(false, name, value)`', function() {
+        this.aim.data({a: [1, 2, 3]});
+        this.aim.data(false, 'a', 2);
+
+        expect( this.aim.data() ).to.eql( {a: [1, 3]} );
+    });
+
+    it('should work for `data()`', function() {
+        this.aim.data({b: 2});
+
+        expect( this.aim.data() ).to.eql( {b: 2} );
+    });
+
+    it('should work for `data(name)`', function() {
+        this.aim.data({a: 3});
+
+        expect( this.aim.data('a') ).to.eql( 3 );
     });
 
 });
